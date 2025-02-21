bd2ba76eddb9bafe558b91ec8f897acba1b3c086
Hassan Raza <hassanrazamughal07@gmail.com>
2025-02-21 15:17:35 +0500

    add 2 new function add and hi

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ console.log(dob);
 const age = 21;
 console.log(age);
 
-<<<<<<< HEAD
-function add(a, b) {    
-    return a + b;
-=======
 function hello(){
     console.log("Hello World");
 }
 function hi(){   
     console.log("Hi World");
->>>>>>> newbranch
 }
